7366c8ca0014ccb54e766c4ea30566913c8cc7c3
Bair <bayr0308@yandex.ru>
2025-08-31 09:45:23 +0300

    fix: replace env

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from "drizzle-kit";
 
-import { config } from "./src/config";
-
 export default defineConfig({
   out: "./src/persistence/db/migrations",
   schema: "./src/persistence/db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: config.databaseUrl,
+    // @ts-ignore
+    url: process.env.DATABASE_URL!,
   },
 });
