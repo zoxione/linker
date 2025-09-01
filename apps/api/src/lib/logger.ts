d6ac3a3e2d366d918da82aa576c2d64111c00e1d
Bair <bayr0308@yandex.ru>
2025-09-01 09:40:41 +0300

    refactor: update logger

--- apps/api/src/lib/logger.ts
+++ apps/api/src/lib/logger.ts
@@ -1,5 +1,13 @@
 import pino from "pino";
 
-const logger = pino({});
+const logger = pino({
+  base: null,
+  timestamp: pino.stdTimeFunctions.isoTime,
+  formatters: {
+    level: (label) => {
+      return { level: label.toUpperCase() };
+    },
+  },
+});
 
 export { logger };
--- apps/api/src/server/core/build-errors.ts
+++ apps/api/src/server/core/build-errors.ts
@@ -1,9 +1,14 @@
 import { HTTPException } from "hono/http-exception";
 
+import { logger } from "../../lib/logger";
 import { Server } from "./build-server";
 
 const buildErrors = async (server: Server) => {
   server.onError(async (error, c) => {
+    logger.error({
+      message: error.message,
+      error,
+    });
     if (error instanceof HTTPException) {
       return c.json(
         {
