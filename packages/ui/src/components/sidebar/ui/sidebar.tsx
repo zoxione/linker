38bc86a47081ebdeafbc085928a684e552ee6dbb
Bair <bayr0308@yandex.ru>
2025-09-02 15:42:06 +0300

    fix: close sidebar on mobile

--- packages/ui/src/components/sidebar/ui/sidebar.tsx
+++ packages/ui/src/components/sidebar/ui/sidebar.tsx
@@ -477,7 +477,7 @@ const SidebarMenuButton = ({
   tooltip?: string | React.ComponentProps<typeof TooltipContent>;
 } & VariantProps<typeof sidebarMenuButtonVariants>) => {
   const Comp = asChild ? Slot : "button";
-  const { isMobile, state } = useSidebar();
+  const { isMobile, state, toggleSidebar } = useSidebar();
 
   const button = (
     <Comp
@@ -485,6 +485,11 @@ const SidebarMenuButton = ({
       data-sidebar="menu-button"
       data-size={size}
       data-active={isActive}
+      onClick={() => {
+        if (isMobile) {
+          toggleSidebar();
+        }
+      }}
       className={cn(sidebarMenuButtonVariants({ variant, size }), className)}
       {...props}
     />
@@ -617,6 +622,7 @@ const SidebarMenuSubButton = ({
   isActive?: boolean;
 }) => {
   const Comp = asChild ? Slot : "a";
+  const { isMobile, toggleSidebar } = useSidebar();
 
   return (
     <Comp
@@ -624,6 +630,11 @@ const SidebarMenuSubButton = ({
       data-sidebar="menu-sub-button"
       data-size={size}
       data-active={isActive}
+      onClick={() => {
+        if (isMobile) {
+          toggleSidebar();
+        }
+      }}
       className={cn(
         "text-sidebar-foreground ring-sidebar-ring hover:bg-sidebar-accent hover:text-sidebar-accent-foreground active:bg-sidebar-accent active:text-sidebar-accent-foreground [&>svg]:text-sidebar-accent-foreground outline-hidden flex h-7 min-w-0 -translate-x-px items-center gap-2 overflow-hidden rounded-md px-2 focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 aria-disabled:pointer-events-none aria-disabled:opacity-50 [&>span:last-child]:truncate [&>svg]:size-4 [&>svg]:shrink-0",
         "data-[active=true]:bg-sidebar-accent data-[active=true]:text-sidebar-accent-foreground",
