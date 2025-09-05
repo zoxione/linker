305f6c1adf87a16423d32b97e0c4162c60d2e8ae
Bair <bayr0308@yandex.ru>
2025-09-05 11:51:41 +0300

    feat: add createdAt in linksTableColumns

--- apps/web/src/widgets/link/links-table/ui/links-table.columns.tsx
+++ apps/web/src/widgets/link/links-table/ui/links-table.columns.tsx
@@ -4,8 +4,10 @@ import Link from "next/link";
 
 import { Badge } from "@repo/ui/components/badge";
 
+import { DATE_TIME_FORMAT } from "@/core/data/constants";
 import { Link as LinkType } from "@/entities/link/model/link.types";
 import { UpdateStatusLinkSwitch } from "@/features/link/update-status-link";
+import { dayjs } from "@/shared/lib/dayjs";
 import { UrlBadge } from "@/shared/ui/url-badge";
 import { ColumnDef } from "@tanstack/react-table";
 
@@ -59,6 +61,14 @@ const linksTableColumns: ColumnDef<LinkType>[] = [
     cell: ({ row }) => {
       const value = row.original.status;
       return <UpdateStatusLinkSwitch id={row.original.id} status={value} />;
+    },
+  },
+  {
+    accessorKey: "createdAt",
+    header: "Дата и время",
+    cell: ({ row }) => {
+      const value = row.original.createdAt;
+      return <div>{dayjs(value).format(DATE_TIME_FORMAT)}</div>;
     },
   },
   {
