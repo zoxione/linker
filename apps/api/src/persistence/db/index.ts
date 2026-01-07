c173d59c864813b18a5e55b5215ba310ccfedbed
Bair <bayr0308@yandex.ru>
2026-01-07 23:52:11 +0300

    refactor: update database connection setup to use Pool

--- apps/api/src/persistence/db/index.ts
+++ apps/api/src/persistence/db/index.ts
@@ -1,12 +1,18 @@
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
 import { config } from "../../config";
 import * as dbSchema from "./schema";
 
+const pool = new Pool({
+  connectionString: config.databaseUrl,
+  max: 20,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 2000,
+});
+
 const db = drizzle({
-  connection: {
-    connectionString: config.databaseUrl,
-  },
+  client: pool,
   schema: dbSchema,
 });
 
